3faa70a3d86bedda4b1c4a99f81bb4ed920b43b5
YusronFauzanN <Yusronsronz7@gmail.com>
2024-07-09 23:44:01 +0700

    feat: add endpoint reservation and master data for table

--- src/master/master.controller.ts
+++ src/master/master.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { MasterService } from './master.service';
 import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { TCreateDeskRequest } from 'src/libs/entities';
@@ -12,5 +20,27 @@ export class MasterController {
   @ApiBody({ type: createTableDto })
   createTable(@Body() payload: TCreateDeskRequest) {
     return this.masterService.createTable(payload);
+  }
+
+  @Get('table')
+  @ApiBody({ type: createTableDto })
+  getTables() {
+    return this.masterService.getTables();
+  }
+
+  @Get('table')
+  @ApiBody({ type: createTableDto })
+  findOneTable(@Param('id') id: string) {
+    return this.masterService.getTable(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() payload: TCreateDeskRequest) {
+    return this.masterService.updateTable(+id, payload);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.masterService.deleteTable(+id);
   }
 }
--- src/master/master.service.ts
+++ src/master/master.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { TCreateDeskRequest } from 'src/libs/entities';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -25,6 +25,80 @@ export class MasterService {
     } catch (error) {
       return {
         message: 'Error',
+        error: error.message,
+      };
+    }
+  }
+
+  async getTables() {
+    try {
+      const desk = await this.prisma.desk.findMany({});
+
+      return {
+        message: 'Success',
+        data: desk,
+      };
+    } catch (error) {
+      return {
+        message: 'Terjadi Kesalahan!',
+        error: error.message,
+      };
+    }
+  }
+
+  async getTable(id: number) {
+    try {
+      const data = await this.prisma.desk.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      return {
+        message: 'Success',
+        data,
+      };
+    } catch (error) {
+      return {
+        message: 'Terjadi Kesalahan',
+        error: error.message,
+      };
+    }
+  }
+
+  async updateTable(id: number, payload: TCreateDeskRequest) {
+    try {
+      const exist = await this.prisma.desk.findFirst({
+        where: {
+          id,
+        },
+      });
+
+      if (exist) {
+        throw new NotFoundException('Data Tidak Ditemukan!');
+      }
+    } catch (error) {
+      return {
+        message: 'Terjadi Kesalahan!',
+        error: error.message,
+      };
+    }
+  }
+
+  async deleteTable(id: number) {
+    try {
+      await this.prisma.desk.delete({
+        where: {
+          id,
+        },
+      });
+
+      return {
+        message: 'Success',
+      };
+    } catch (error) {
+      return {
+        message: 'Terjadi Kesalahan!',
         error: error.message,
       };
     }
